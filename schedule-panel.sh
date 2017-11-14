08633a7f8f4e443a88395dd8db6d739dd2f2fffd
Clinton Reeder <clinton_reeder@byu.edu>
2017-11-14 11:58:08 -0700

    Removed all of the .profile export
    Removed all of the .profile export out of the script so it stops adding the export over and over again in .profile.

--- schedule-panel.sh
+++ schedule-panel.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-echo "Setting up Go evironmental variables"
-echo "" >> ~/.profile
-echo "# golang" >> ~/.profile
-echo "export GOPATH=/home/pi/go" >> ~/.profile 
-echo "export GOSRC=/home/pi/go/src" >> ~/.profile
-echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.profile
-
 cd /home/pi/go/src/github.com/byu-oit/av-scheduling-ui
 
 /usr/local/go/bin/go run server.go
